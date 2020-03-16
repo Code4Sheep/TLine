90757cf2e638a1d0cce604342491cb595ebffc69
Code4Sheep <jar.guinto@gmail.com>
2020-03-16 15:17:42 -0500

    'Release' Version, Removed debugging lines and cleaned up output

--- coolFuncs.py
+++ coolFuncs.py
@@ -1,7 +1,4 @@
 import math
-import random
-
-
 
 class Node:
     #   a representation of Nodes
@@ -24,9 +21,7 @@ class Node:
         return self.strip
     def nodePrint(self):
         return "p: " + str.format('{0:1.4f}', self.pot) + ", r: " + str.format('{0:1.4f}', math.fabs(self.res)) + ", i: " + str.format('{0:1.1f}',int(self.interface))
-        #return "p: " + str(self.pot) + ", r: " + str(self.res)
-        #return "p: " + str(self.pot) + ", r: " + str(self.res) + ", i: " + str(int(self.interface)) \
-        #       + ", s: " + str(int(self.strip))
+
 
 class customReturn:
     #custom return function returns a matrix and additonal value
@@ -44,29 +39,22 @@ class customReturn:
 
 def initNodeMatrix (wd,bd,aw,na,nb):
     #   makes the initial matrix, how nice
-
     #   assume a = 1 and derive the rest of the values
     a = 1
     w = float(a / aw)
     d = float(w / wd)
     b = float(d * bd)
 
-    #just checking
-    print("a: " + str(a) + " w: " + str(w) + " d: " + str(d) + " b: " + str(b))
     #derive h and k values, usefull to check if a node is on the strip or not
     k = float(b/(nb-1))
     h = float(a/(na-1))
 
     #   calc where strip is relative to top left as well as calc how long the strip is in terms of nodes
-    #
     ind = float(b - d)
     interfaceY = math.floor(ind/k)
-    print("interface y: " + str(interfaceY))
     stripThreshX = math.floor(w/h)
-    print("strip x: " + str(stripThreshX))
     #   generate matrix skeleton
     nodes = [[Node(0, 0, False, False) for j in range(na)] for i in range(nb)]
-
 
     #   apply values to special cases, strip and
     for x in range(0, na):
@@ -75,13 +63,9 @@ def initNodeMatrix (wd,bd,aw,na,nb):
         else:
             nodes[interfaceY][x] = Node(0, 0, True, False)
 
-
     shell = customReturn([],stripThreshX,interfaceY)
     ret = customReturn(nodes, float(h/k),shell)
-
-
     return ret
-
 
 def calcNode(mat, row, col, alpha, relaxation, Er):
     #   Calculates a new node from the old node
@@ -89,7 +73,6 @@ def calcNode(mat, row, col, alpha, relaxation, Er):
 
     #   if the node is the strip then just pass over it
     if mat[row][col].isStrip():
-
         return mat[row][col]
 
     top = mat[row - 1][col]
@@ -98,37 +81,24 @@ def calcNode(mat, row, col, alpha, relaxation, Er):
     left = Node(0, 0, False, False)
     if col == 0:
         left = right
-
     else:
         left = mat[row][col - 1]
 
+    #check interface condition
     if mat[row][col].isInterface():
         E = Er
     else:
         E = 1
 
-    #print(str(row) + " " +str(col) + " - " + str(mat[row][col].isInterface()) + " Er to use " + str(E))
     a2 = float(alpha*alpha)
     A = 1 / (2 * (1 + a2))
     B = left.getPot() + right.getPot()
     C = (2*a2)/(1+E)
     D = top.getPot() + E*bottom.getPot()
 
-    #My way but not SOR way
-    #newPot = A * (B + C*D)
-    #if (row == 3 and col == 0):
-    #    print(str(A))
-    #    print(str(B))
-    #    print(str(C))
-    #    print(str(D))
-    #    print(str(newPot))
-    #oldPot = mat[row][col].getPot()
-    #Res = (newPot - oldPot) / relaxation
-
     #implement sor algo
     Res = A * (B + C*D) - mat[row][col].getPot()
     newPot = mat[row][col].getPot() + relaxation*Res
-
     ret = Node(newPot, Res, mat[row][col].isInterface(), mat[row][col].isStrip())
     return ret
 
@@ -142,13 +112,11 @@ def reRoll(mat,na,nb,resMax, alpha , relaxation, Er):
                 mat[row][col] = calcNode(mat, row, col, alpha, relaxation, Er)
                 if mat[row][col].getRes() > resMax:
                     resMax = mat[row][col].getRes()
-                #print(str(row) + " " + str(col))
         else:
             for col in range(na - 2, -1, -1):
                 mat[row][col] = calcNode(mat, row, col, alpha, relaxation, Er)
                 if mat[row][col].getRes() > resMax:
                     resMax = mat[row][col].getRes()
-                #print(str(row) + " " + str(col))
 
     ret = customReturn(mat,resMax,0)
     return ret
@@ -163,82 +131,45 @@ def contourCalc(mat,stripthreshX,interfaceY,na,nb,alpha,Er):
     offsetBot = 1
     offsetRight = 1
 
-    #if interfaceY - 2 > 0:
-    #    offsetTop = 2
-    #if interfaceY + 2 < nb-1:
-    #    offsetBot = 2
-    #if stripthreshX  + 1  < na-1:
-    #    offsetRight = 2
 
     topoffset = interfaceY - offsetTop
     botoffset = interfaceY + offsetBot
     rightoffset = stripthreshX + offsetRight
-    #print(str(topoffset))
-    #print(str(botoffset))
-    #print(str(rightoffset))
+
 
     #sum bottom and top leg
-    print("topbot")
     for col in range(0, rightoffset+1):
-        #print("bot: nb-1 : " + str(mat[botoffset+1][col].getPot()) + " nb+1: " + str(mat[botoffset-1][col].getPot()))
         tempbot = Er*(mat[botoffset+1][col].getPot() -
                       mat[botoffset-1][col].getPot())
-        #print("top: na+1 : " + str(mat[topoffset-1][col].getPot()) + " na-1: " + str(mat[topoffset+1][col].getPot()))
         temptop = (mat[topoffset-1][col].getPot() -
                    mat[topoffset+1][col].getPot())
 
         if (col == 0 or col == rightoffset):
-            #print("term:" + str(col) + " , summing term: " + str(tempbot/4))
             sumtop = sumtop + (1/2)*temptop
             sumbot = sumbot + (1 / 2) * tempbot
             sumtopbot = sumtopbot + (1/2)*(temptop + tempbot)
-            #print("div by 2")
         else:
             sumtop = sumtop + temptop
             sumbot = sumbot + tempbot
             sumtopbot = sumtopbot + (temptop + tempbot)
-            #print("no div")
 
-
-
-
-    #print("sumbot: " + str(Er) + ": " + str(sumbot))
-    #print("sumtop: " + str(Er) + ": " + str(sumtop))
-    #print("sumtopbot: "+ str(Er) + ": " + str(sumtopbot))
-
-
-    #print("right top")
     for row in range(topoffset,interfaceY+1):
-
-        #print("right top: mr+1 : " + str(mat[row][rightoffset].getPot()) + " mr-1: " + str(mat[row][rightoffset-2].getPot()))
         tempRT = (mat[row][rightoffset+1].getPot() -
                       mat[row][rightoffset-1].getPot())
         if (row == topoffset or row == interfaceY):
-            #print("term:" + str(row) + " , summing term: " + str(tempRT/4))
             sumRT = sumRT + (1/2)*tempRT
-            #print("div by 2")
         else:
             sumRT = sumRT + tempRT
-            #print("no div")
-    #print("sumRT:" +str(Er) + ": " + str(sumRT))
 
-    #print("right bot")
     for row in range(interfaceY,botoffset+1):
-        #print("right bot: mr+1 : " + str(mat[row][rightoffset].getPot()) + " mr-1: " + str(mat[row][rightoffset - 2].getPot()))
         tempRB =Er*(mat[row][rightoffset+1].getPot() -
                       mat[row][rightoffset-1].getPot())
         if (row == interfaceY or row == botoffset):
             sumRB = sumRB + (1/2)*tempRB
-            #print("div by 2")
         else:
             sumRB = sumRB + tempRB
-            #print("no div")
-    #print("sumRB:" + str(Er) + ": " + str(sumRB))
 
-    print(str(sumtopbot) + " " + str(sumRT+sumRB))
     negCEo = alpha*sumtopbot + (1/alpha)*(sumRT+sumRB)
-    print()
-
     return negCEo
 
 
--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from mpl_toolkits.mplot3d import axes3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-#test for git
 wd = float(input("enter wd: "))
 bd = float(input("enter bd: "))
 aw = float(input("enter aw: "))
@@ -15,38 +12,41 @@ nb = int(input("enter nb: "))
 Er = float(input("enter Er: "))
 customRelax = str(input("custom relaxation parameter? (T/F): "))
 gr = str(input("3D graph? (T/F): "))
+if gr == "T":
+    print("1 for Air Line")
+    print("2 for Substrate")
+    print("3 for Both")
+    choice = int(input())
+else:
+    choice = 0
 if customRelax == "T":
     relaxation = float(input("enter relaxation: "))
 else:
     relaxation = 2 * (1 - (math.pi / math.sqrt(2)) * math.sqrt((1 / math.pow(na, 2)) + (1 / math.pow(nb, 2))))
 grab = initNodeMatrix(wd,bd,aw,na,nb)
 nodes = grab.getMat()
-
 alpha = grab.getAdditional1()
 temp = grab.getAdditional2()
 stripThreshX = temp.getAdditional1()
 interfaceY = temp.getAdditional2()
+print()
+print("relaxation: " + str.format('{0:1.4f}', relaxation))
+print("alpha : " + str.format('{0:1.4f}', alpha))
 
-print("relaxation: " + str(relaxation))
+#setup for potential calculation
 resMax = 100
 itER = 0
 itAIR = 0
-#interface y is zero indexed but strip x isnt
-
 while resMax > pow(10, -5) and itER < 1000:
     resMax = 0
     ret = reRoll(nodes, na, nb, resMax, alpha, relaxation, Er)
     nodesEr = ret.getMat()
     resMax = ret.getAdditional1()
     itER = itER + 1
-    if (itER % 100 == 0):
-        print("* ", end="")
-    #    print("resMax: " + str(resMax))
 
 grab = initNodeMatrix(wd, bd, aw, na, nb)
 nodes = grab.getMat()
 resMax = 100
-
 while resMax > pow(10, -5) and itAIR < 1000:
     resMax = 0
     ret = reRoll(nodes, na, nb, resMax, alpha, relaxation, 1)
@@ -54,29 +54,27 @@ while resMax > pow(10, -5) and itAIR < 1000:
     resMax = ret.getAdditional1()
     itAIR = itAIR + 1
 
-
+#these arrays are for the graphics
 AirX = []
 AirY = []
 AirZ = []
+ErX = []
+ErY = []
+ErZ = []
 
 for row in range(0, nb):
-    temp = ""
     for col in range(0, na):
-        pass
-        temp = temp + " | " + nodesEr[row][col].nodePrint()
-    print(temp + " |")
-print("ER: " + str(itER))
+        ErX.append(col)
+        ErY.append(row)
+        ErZ.append(nodesEr[row][col].getPot())
+print("Iterations (Er): " + str(itER))
 
 for row in range(0, nb):
-    temp = ""
     for col in range(0, na):
-        temp = temp + " | " + nodesAir[row][col].nodePrint()
         AirX.append(col)
         AirY.append(row)
         AirZ.append(nodesAir[row][col].getPot())
-    print(temp + " |")
-
-print("Air: " + str(itAIR))
+print("Iterations (Air): " + str(itAIR))
 
 
 CEoAir = contourCalc(nodesAir,stripThreshX,interfaceY,na,nb,alpha,1)
@@ -84,24 +82,40 @@ CEoEr = contourCalc(nodesEr,stripThreshX,interfaceY,na,nb,alpha,Er)
 Eo = float(pow(10, -9)/(36*math.pi))
 CAir = CEoAir*Eo*-1
 CEr = CEoEr*Eo*-1
-#print(str(AirZnp))
-print("C/Eo air: " +str(CEoAir))
-print("C/Eo Er: " +str(CEoEr))
-print(str(CAir))
-print(str(CEr))
+print("C/Eo Air: " +str.format('{0:1.4f}', CEoAir))
+print("C/Eo Er: " +str.format('{0:1.4f}', CEoEr))
 
-if gr == "T":
-    AirZ1D = np.array(AirZ)
-    AirZ = [AirZ, AirZ]
+if choice == 1 or choice == 3:
     AirXnp = np.array(AirX)
     AirYnp = np.array(AirY)
     AirZnp = np.array(AirZ)
+
+
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection ='3d')
-    ax.set_xlim(na,0)
-    ax.set_ylim(nb,0)
-    ax.set_xlabel('na')
-    ax.set_ylabel('nb')
-    ax.set_zlabel('pot')
-    ax.plot_trisurf(AirXnp,AirYnp,AirZ1D)
+    ax1 = fig.add_subplot(111, projection ='3d')
+
+    ax1.title.set_text("Air Line")
+    ax1.set_xlim(na,0)
+    ax1.set_ylim(nb,0)
+    ax1.set_xlabel('na')
+    ax1.set_ylabel('nb')
+    ax1.set_zlabel('pot')
+    ax1.plot_trisurf(AirXnp,AirYnp,AirZnp)
+    plt.show()
+
+if choice == 2 or choice == 3:
+    ErXnp = np.array(ErX)
+    ErYnp = np.array(ErY)
+    ErZnp = np.array(ErZ)
+
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111, projection='3d')
+
+    ax2.title.set_text("Er = " + str(Er))
+    ax2.set_xlim(na, 0)
+    ax2.set_ylim(nb, 0)
+    ax2.set_xlabel('na')
+    ax2.set_ylabel('nb')
+    ax2.set_zlabel('pot')
+    ax2.plot_trisurf(ErXnp, ErYnp, ErZnp)
     plt.show()
